f028f10931eff3b8ca70f19aaf5951136f79720f
Eduardo Keenan Moreira <eduardo.keenan.moreira@gmail.com>
2023-03-30 16:22:35 -0300

    ajustado o config do dotenv

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const rotas = require(`./router`);
 const app = express();
-require('dotenv/config');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
